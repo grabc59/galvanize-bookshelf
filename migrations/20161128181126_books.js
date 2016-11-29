0fd3b493400a7ce8a56ac90aa7497b8c48e7c200
Christopher Grabski <grabc59@gmail.com>
2016-11-29 10:39:25 -0700

    part 1 seeded, and last test passing

---
+++ migrations/20161128181126_books.js
@@ -0,0 +1,24 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('books', function(table) {
+    // id
+    table.increments();
+    // title
+    table.string('title').notNullable().defaultTo('');
+    // author
+    table.string('author').notNullable().defaultTo('');
+    // genre
+    table.string('genre').notNullable().defaultTo('');
+    // description
+    table.text('description').notNullable().defaultTo('');
+    // cover_url
+    table.text('cover_url').notNullable().defaultTo('');
+    // created_at, updated_at
+    table.timestamps();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('books');
+};
