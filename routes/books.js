8ad4db76dd895c4692c16b589816bb2d05968754
Christopher Grabski <grabc59@gmail.com>
2016-12-01 09:02:50 -0700

    part 2 bonuses complete

--- routes/books.js
+++ routes/books.js
@@ -3,115 +3,172 @@
 const express = require('express');
 const router = express.Router(); // express.Router can be used to create modular, mountable route handlers. AKA mini app
 const knex = require('../knex');
-// const boom = require('boom'); // error logging module
+const boom = require('boom'); // error logging module
 const morgan = require('morgan'); // req/res logging module
-const { camelizeKeys, decamelizeKeys } = require('humps'); // camel case module to properly format results for tests. special es6 syntax
+const {
+    camelizeKeys,
+    decamelizeKeys
+} = require('humps'); // camel case module to properly format results for tests. special es6 syntax
 
 router.use(morgan('short')); // use morgan and format it's output
 
-
-
 // eslint-disable-next-line new-cap
 
-
 router.get('/books', function(req, res, next) {
-  knex('books')
-    .orderBy('title')
-    .then((result) => {
-      const book = camelizeKeys(result);
-      res.send(book);
-    })
-    .catch((err) => {
-      next(err);
-    });
+    knex('books')
+        .orderBy('title')
+        .then((result) => {
+            const book = camelizeKeys(result);
+            res.send(book);
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
 
-router.get('/books/:id', function(req,res, next) {
-  knex('books')
-    .max('id')
-    .then((result) => {
-      // console.log(result[0]);
-      return knex('books')
-        .where('id', req.params.id)
-        .first()
+router.get('/books/:id', function(req, res, next) {
+    knex('books')
+        .max('id')
+        .then((result) => {
+            // console.log(result[0]); // anonymous { max: 12 }
+            // test for out of bounds and valid type id input
+            if (req.params.id <= result[0].max && req.params.id > 0 && !isNaN(req.params.id)) {
+                return knex('books')
+                    .where('id', req.params.id)
+                    .first()
+                    .then((result) => {
+                        if (!result) {
+                            return next();
+                        }
+                        const book = camelizeKeys(result);
+                        res.send(book);
+                    })
+                    .catch((err) => {
+                        next(err);
+                    });
+            } else {
+                next(boom.create(404, 'Not Found'));
+                return;
+            }
+        })
+});
 
-      .then((result) => {
-        if (!result) {
-          return next();
+
+router.post('/books', (req, res, next) => {
+    const {
+        title,
+        author,
+        genre,
+        description,
+        coverUrl
+    } = req.body;
+    if (title && author && genre && description && coverUrl) {
+        knex('books')
+            .insert({
+                title: req.body.title,
+                author: req.body.author,
+                genre: req.body.genre,
+                description: req.body.description,
+                cover_url: req.body.coverUrl
+            }, '*')
+            .then((result) => {
+                const book = camelizeKeys(result[0]);
+                res.send(book);
+            })
+            .catch((err) => {
+                next(err);
+            });
+    } else {
+        // chose not to break this out into a function because this is specific to the 'post' requests for books.
+        if (!title) {
+            next(boom.create(400, 'Title must not be blank'));
+            return;
         }
-        const book = camelizeKeys(result);
-        res.send(book);
-      })
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
-
-
-router.post('/books', (req,res,next) => {
-   knex('books')
-    .insert({
-      title: req.body.title,
-      author: req.body.author,
-      genre: req.body.genre,
-      description: req.body.description,
-      cover_url: req.body.coverUrl
-    }, '*')
-    .then((result) => {
-
-      const book = camelizeKeys(result[0]);
-      res.send(book);
-   })
-     .catch((err) => {
-     next(err);
-   });
+        if (!author) {
+            next(boom.create(400, 'Author must not be blank'));
+            return;
+        }
+        if (!genre) {
+            next(boom.create(400, 'Genre must not be blank'));
+            return;
+        }
+        if (!description) {
+            next(boom.create(400, 'Description must not be blank'));
+            return;
+        }
+        if (!coverUrl) {
+            next(boom.create(400, 'Cover URL must not be blank'));
+            return;
+        }
+    }
 });
 
 
 
 router.patch('/books/:id', (req, res, next) => {
-  return knex('books')
-    .where('id', req.params.id)
-    .first()
-    .update({
-      title: req.body.title,
-      author: req.body.author,
-      genre: req.body.genre,
-      description: req.body.description,
-      cover_url: req.body.coverUrl
-    }, '*')
-    .then((result) => {
-      const book = camelizeKeys(result[0]);
-      res.send(book);
-    })
-    .catch((err) => {
-      next(err);
-    });
+    return knex('books')
+        .max('id')
+        .then((result) => {
+            // check for out of bounds and valid type id input
+            if (req.params.id <= result[0].max && req.params.id > 0 && !isNaN(req.params.id)) {
+                return knex('books')
+                    .where('id', req.params.id)
+                    .first()
+                    .update({
+                        title: req.body.title,
+                        author: req.body.author,
+                        genre: req.body.genre,
+                        description: req.body.description,
+                        cover_url: req.body.coverUrl
+                    }, '*')
+                    .then((result) => {
+                        const book = camelizeKeys(result[0]);
+                        res.send(book);
+                    })
+                    .catch((err) => {
+                        next(err);
+                    });
+            } else {
+                next(boom.create(404, 'Not Found'));
+                return;
+            }
+        });
 });
 
+
+
 router.delete('/books/:id', (req, res, next) => {
-  let book;
-  knex('books')
-  .where('id', req.params.id)
-  .first()
-  .then((result) => {
-    if (!result) {
-      return next();
-    }
-    book = camelizeKeys(result);
     return knex('books')
-      .del()
-      .where('id', req.params.id);
-  })
-  .then (() => {
-    delete book.id;
-    res.send(book);
-  })
-  .catch((err) => {
-    next(err);
-  });
+        .max('id')
+        .then((result) => {
+            // check for out of bounds and valid type id input
+            if (req.params.id <= result[0].max && req.params.id > 0 && !isNaN(req.params.id)) {
+                let book;
+                knex('books')
+                    .where('id', req.params.id)
+                    .first()
+                    .then((result) => {
+                        if (!result) {
+                            return next();
+                        }
+                        book = camelizeKeys(result);
+                        return knex('books')
+                            .del()
+                            .where('id', req.params.id);
+                    })
+                    .then(() => {
+                        delete book.id;
+                        res.send(book);
+                    })
+                    .catch((err) => {
+                        next(err);
+                    });
+            } else {
+                next(boom.create(404, 'Not Found'));
+                return;
+            }
+        });
 });
 
 
