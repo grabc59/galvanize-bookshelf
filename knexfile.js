00812284c7019d7f7ab82dd4c8832d271d5a7147
Christopher Grabski <grabc59@gmail.com>
2016-12-04 23:53:26 -0700

    add pool config for heroku

--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,20 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/bookshelf_dev'
+    connection: 'postgres://localhost/bookshelf_dev',
+    pool: {
+      min: 0,
+      max: 7
+    }
   },
 
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/bookshelf_test'
+    connection: 'postgres://localhost/bookshelf_test',
+    pool: {
+      min: 0,
+      max: 7
+    }
   },
 
   production: {}
